202a8def85df3c3ee41c7b1e95fc6d7c4d3b01b9
Niranjan Rai <rainiranjan1998@gmail.com>
2023-05-12 08:02:22 +0530

    some update

--- App.js
+++ App.js
@@ -3,14 +3,56 @@ import { createRoot } from "react-dom/client";
 
 const el = document.getElementById("root");
 
-const App = () => {
-  const name = "Niranjan Rai";
-  console.log(name);
+const Title = () => {
   return (
     <>
-      <h2>Hello world</h2>
+      <h1>Food Villa</h1>
     </>
   );
 };
 
-createRoot(el).render(<App />);
+const HeaderComponent = () => {
+  return (
+    <header>
+      <Title />
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact </li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      {/**
+       * Header
+          - Logo
+          - NavItems (Right Side)
+          - Cart
+       * Body
+       *  -SearchBar
+       *  -RestuarantList
+       *      - RestuarantCard
+       *          - Image
+       *          - Name
+       *          - Rating
+       *          - Cusiness
+       * Footer
+           - Links
+           - Copyright (c)
+       *
+       */}
+
+      <HeaderComponent />
+    </>
+  );
+};
+
+createRoot(el).render(<AppLayout />);
